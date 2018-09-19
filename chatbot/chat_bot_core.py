474bfb8d30fa3cf1c5e361642e26c73eec09f240
KayafasKain <bismark9988@gmail.com>
2018-09-19 19:50:58 +0300

    Info command provided

--- chatbot/chat_bot_core.py
+++ chatbot/chat_bot_core.py
@@ -16,29 +16,57 @@ class ChatBotCore:
     Handling bot activities
     '''
 
-    commands = [
-        '/info',
-        '/register',
-        '/newloan'
-    ]
-
     fields = {
         'seniority': 'Work seniority. Represented in years',
         'home': 'Home ownership. (rent, owner, partners, private, other, ignore)',
         'time': 'Time of loan, represented in months.',
         'age': 'Loaner age. Represented in years.',
         'marital': 'Marital status. (married. widow, single, separated, divorced)',
-        'records': 'Represents other loans',
+        'records': 'Represents other loans (yes, no)',
         'job': 'Represents type of job. (freelance, fixed, partime, others)',
         'expenses': 'Represents monthly expenses',
         'income': 'Represents monthly income',
         'assets': 'Represents assets',
         'amount': 'Loan amount',
         'price': 'Loan price',
-        'finrat': 'Finantial rating',
-        'savings': 'Loaner`s savings'
     }
-
+    messages = {
+        'register': {
+            'msg': {
+                'registrations_success': 'Nice! Thank you for registration! You can check your credit score now!',
+                'provide_phone': 'Nice! Now, tell me your mobile phone, such as: /register phone: +380NNNNNNNNN',
+                'provide_fullname': 'Tell me your full name, such as: "/register name: Ivanov Ivan"'
+            },
+            'err': {
+                'incorrect_phone': 'Please, tell me correct phone number',
+                'incorrect_fullname': 'Please, tell me correct full name',
+            }
+        },
+        'err_common': {
+            'wrong_data': 'Please enter correct data!',
+            'unknown_error': 'Something went wrong',
+        },
+        'loan': {
+            'loan_approved': 'You can take these loan',
+            'loan_disapproved': 'I can not classify you, please contact humans',
+            'new': {
+                'seniority': 'Tell me about your seniority! "/newloan seniority: 21"',
+                'price': 'Nice! Now tell me, how many you expect to return. "/newloan price: 1040"',
+                'amount': 'Nice! Now tell me, how many you expect to get. "/newloan amount: 1000"',
+                'debt': 'Nice! Now tell me, summ of your debts. "/newloan debt: 0"',
+                'assets': 'Nice! Now tell me about your assets "/newloan assets: 1000"',
+                'income': 'Nice! Now tell me about your income. /newloan income: 1000"',
+                'expenses': 'Nice! Now tell me about your expenses! "/newloan expenses: 1000"',
+                'job': 'Nice! Now tell me about your job! "/newloan job: freelance"',
+                'home': 'Nice!Now, tell me about your home. /newloan home:' + fields['home'],
+                'time': 'How long will it take for you, to pay out the loan?(months) "/newloan time: 12"',
+                'age': 'Nice! How old are you? "/newloan age: 54"',
+                'marital': 'Nice! Are you merried? "/newloan marital: married "',
+                'records': 'Nice! Are you have any credit records? "/newloan records: yes"',
+                'score': 'Done! Now you can check credit chance! "/score last"'
+            }
+        }
+    }
     home_ownership = {
         'rent': 0,
         'owner': 1,
@@ -65,6 +93,7 @@ class ChatBotCore:
         'others': 3
     }
 
+
     def __init__(self, request, TelegramBot):
         '''
         Taking request object from MessageLoop
@@ -82,7 +111,7 @@ class ChatBotCore:
         if '/register' in request_text:
             self.register_sequence(request_text)
         elif '/info' in request_text:
-            self.info_sequence()
+            self.info_sequence(request_text)
         elif '/newloan' in request_text:
             self.newloan_seqence(request_text)
         elif '/score last' in request_text:
@@ -97,7 +126,7 @@ class ChatBotCore:
         elif 'phone' in request_text:
             self.reg_phone(request_text)
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Tell me your full name, such as: "/register name: Ivanov Ivan"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['register']['msg']['provide_fullname'])
 
     def reg_phone(self, request_text):
         '''
@@ -110,11 +139,9 @@ class ChatBotCore:
             consumer.phone_number = phone[0]
             consumer.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, tell me correct phone number')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['register']['err']['incorrect_phone'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id,
-                                         'Nice! Thank you for registration! You can check your credit score now!'
-                                         )
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['register']['err']['registrations_success'])
 
     def reg_name(self, request_text):
         '''
@@ -132,12 +159,37 @@ class ChatBotCore:
             tel_prof.save()
 
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, tell me correct full name')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['register']['err']['incorrect_fullname'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id,
-                                         'Nice! Now, tell me your mobile phone, such as: /register phone:' +
-                                         ' +380NNNNNNNNN'
-                                         )
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['register']['err']['provide_phone'])
+
+    def info_sequence(self, request_text):
+        if 'seniority' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['seniority'])
+        elif 'home' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['home'])
+        elif 'time' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['time'])
+        elif 'age' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['age'])
+        elif 'marital' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['marital'])
+        elif 'records' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['records'])
+        elif 'job' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['job'])
+        elif 'expenses' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['expenses'])
+        elif 'income' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['income'])
+        elif 'assets' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['assets'])
+        elif 'amount' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['amount'])
+        elif 'price' in request_text:
+            self.TelegramBot.sendMessage(self.chat_id, self.fields['price'])
+        else:
+            self.TelegramBot.sendMessage(self.chat_id, 'I cannot understand you')
 
     def newloan_seqence(self, request_text):
         if  'create' in request_text:
@@ -190,9 +242,9 @@ class ChatBotCore:
 
         status_ml = json.loads(response.read().decode(encoding))['status_ml']
         if status_ml == 1:
-            self.TelegramBot.sendMessage(self.chat_id, 'Gratz! You can take these loan!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['loan_approved'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'I am not certain about you, please contact humans instead.')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['loan_disapproved'])
         cr_req.status_ml=status_ml
         cr_req.save()
 
@@ -203,9 +255,9 @@ class ChatBotCore:
             cr_req.consumer = consumer
             cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Something went wrong...')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['unknown_error'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Tell me about your seniority! "/newloan seniority: 21"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['seniority'])
 
     def nl_amount(self, request_text):
         '''
@@ -216,10 +268,9 @@ class ChatBotCore:
             self.cr_req.amount = int(amount)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please enter correct data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id,
-                                         'Nice! Now tell me, how many you expect to return. "/newloan price: 1040"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['price'])
 
     def nl_debt(self, request_text):
         '''
@@ -230,10 +281,9 @@ class ChatBotCore:
             self.cr_req.debt = int(debt)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please enter correct data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id,
-                                         'Nice! Now tell me, how many you expect to get. "/newloan amount: 1000"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['amount'])
 
     def nl_assets(self, request_text):
         '''
@@ -244,9 +294,9 @@ class ChatBotCore:
             self.cr_req.assets = int(assets)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please enter correct data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Now tell me, summ of your debts. "/newloan debt: 0"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['debt'])
 
     def nl_income(self, request_text):
         '''
@@ -257,9 +307,9 @@ class ChatBotCore:
             self.cr_req.income = int(income)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, provide with valid income data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Now tell me about your assets "/newloan assets: 1000"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['assets'])
 
     def nl_expenses(self, request_text):
         '''
@@ -270,9 +320,9 @@ class ChatBotCore:
             self.cr_req.expenses = int(expenses)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, provide me with valid dataa')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Now tell me about your income.')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['income'])
 
     def nl_job(self, request_text):
         '''
@@ -283,9 +333,9 @@ class ChatBotCore:
             self.cr_req.job = self.job_status[job]
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please provide me with correct data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Now tell me about your expenses! "/newloan expenses: 1000"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['expenses'])
 
     def nl_records(self, request_text):
         '''
@@ -296,9 +346,9 @@ class ChatBotCore:
             self.cr_req.records = self.records_status[records]
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'So, have you any records? Yes or no?')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Now tell me about your job! "/newloan job: freelance"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['job'])
 
     def nl_seniority(self, request_text):
         '''
@@ -309,11 +359,9 @@ class ChatBotCore:
             self.cr_req.seniority = int(seniority)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, tell me correct info about your seniority')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice!Now, tell me about your home. /newloan home:' +
-                                         self.fields['home']
-                                         )
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['home'])
 
     def nl_home(self, request_text):
         '''
@@ -324,11 +372,9 @@ class ChatBotCore:
             self.cr_req.home = int(self.home_ownership[home])
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, provide me with correct data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'How long will it take for you, to pay out the loan?(months)' +
-                                        '"/newloan time: 12"'
-                                         )
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['time'])
 
     def nl_time(self, request_text):
         '''
@@ -339,9 +385,9 @@ class ChatBotCore:
             self.cr_req.time = int(time)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please give me correct time!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Now tell me, how old are you?')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['age'])
 
     def nl_age(self, request_text):
         '''
@@ -352,9 +398,9 @@ class ChatBotCore:
             self.cr_req.age = int(age)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, don`t hide your real age from me!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Are you merried? "/newloan marital: married "')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['marital'])
 
     def nl_marital(self, request_text):
         '''
@@ -365,9 +411,9 @@ class ChatBotCore:
             self.cr_req.marital = self.marital_status[marita]
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please, provide me with valid marital data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id, 'Nice! Are you have any credit records? "/newloan records: yes"')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['records'])
 
     def nl_price(self, request_text):
         '''
@@ -378,12 +424,8 @@ class ChatBotCore:
             self.cr_req.price = int(price)
             self.cr_req.save()
         except:
-            self.TelegramBot.sendMessage(self.chat_id, 'Please enter correct data!')
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['err_common']['wrong_data'])
         else:
-            self.TelegramBot.sendMessage(self.chat_id,
-                                         'Done! Now you can check credit chance! "/score last"')
-
-    def info_sequence(self):
-        pass
+            self.TelegramBot.sendMessage(self.chat_id, self.messages['loan']['new']['score'])
 
 
